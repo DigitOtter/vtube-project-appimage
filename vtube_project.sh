c9023b6315b5b2c5912568d4b4cd78914b45d9a8
DigitOtter <digitotter@protonmail.com>
2024-07-09 21:52:18 +0200

    Initial commit

---
+++ vtube_project.sh
@@ -0,0 +1,10 @@
+#!/bin/env sh
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+PATH="$SCRIPT_DIR/usr/bin"${PATH:+:${PATH}}
+LD_LIBRARY_PATH="$SCRIPT_DIR/usr/lib"${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+VTUBE_PROJECT_DIR="$SCRIPT_DIR/usr/share/vtube_project"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$VTUBE_PROJECT_DIR"
+exec "$SCRIPT_DIR/usr/bin/godot.linuxbsd.template_release.x86_64" --main-pack "$VTUBE_PROJECT_DIR/vtube_project.pck" "$@"
